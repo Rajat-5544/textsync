01ad007fec28681e74d0593c2b484c14291d1a36
Rajat <21ucc081@lnmiit.ac.in>
2024-12-28 22:13:24 +0530

    Update Client URL

--- textsync-server/src/server.ts
+++ textsync-server/src/server.ts
@@ -19,7 +19,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://textsync.vercel.app",
     methods: ["GET", "POST"],
   },
 });
